20052d7d5e7a6f01d1051f0f7cd00026d8545121
viashchuk <victoriya.dev@icloud.com>
2025-03-31 09:30:11 +0200

    Add quick sort

--- task-1/RandomGenerator.pas
+++ task-1/RandomGenerator.pas
@@ -1,7 +1,12 @@
 program RandomGenerator;
 
+const
+  array_size = 50;
+  min = 0;
+  max = 100; 
+
 type
-    numbers = array[integer] of 1..50;
+    numbers = array[1..array_size] of Integer; 
 var
     random_numbers: numbers;
     j: integer;
@@ -13,16 +18,54 @@ var
 begin
     randomize;
 
-    for i := 1 to 50 do begin
-        x := random(100-0+1) + 0;
+    for i := 1 to array_size do begin
+        x := random(max-min+1) + min;
         random_numbers[i] := x
     end;
 end;
 
 
+procedure QuickSort(l, r: Integer);
+var 
+  newL, newR: Integer;
+  temp, pivot: Integer;
+begin
+  newL := l;
+  newR := r;
+  
+  pivot := random_numbers[(l + r) div 2];
+  
+  repeat
+    while random_numbers[newL] < pivot do
+      newL := newL + 1;
+
+    while random_numbers[newR] > pivot do
+      newR := newR - 1;
+
+    if newL <= newR then
+    begin
+      { Исправление обмена элементов }
+      temp := random_numbers[newL];
+      random_numbers[newL] := random_numbers[newR];
+      random_numbers[newR] := temp;
+
+      newL := newL + 1;
+      newR := newR - 1;
+    end;
+  until newL > newR;
+
+  if l < newR then
+    QuickSort(l, newR);
+
+  if newL < r then
+    QuickSort(newL, r);
+end;
+
+
 begin
     GenerateRandomNumbers;
+    QuickSort(1, array_size);
 
-    for j:= 1 to 50 do
+    for j:= 1 to array_size do
         write(random_numbers[j], ' ');
 end.
