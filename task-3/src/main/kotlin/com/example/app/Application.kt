40f9485f30a929900fc8a1b21fdac9db3ff005d3
viashchuk <victoriya.dev@icloud.com>
2025-04-17 12:41:08 +0200

    Init Spring

---
+++ task-3/src/main/kotlin/com/example/app/Application.kt
@@ -0,0 +1,12 @@
+package com.example.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+    println("Hello World!")
+}
