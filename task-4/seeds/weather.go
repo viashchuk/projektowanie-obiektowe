86fb97d0400ff95e30e54ebd7d87747384693e20
viashchuk <victoriya.dev@icloud.com>
2025-04-25 15:55:25 +0200

    Update Seed

--- task-4/seeds/weather.go
+++ task-4/seeds/weather.go
@@ -7,12 +7,9 @@ import (
 
 func (s *Seed) SeedWeather() {
 	weather := []models.Weather{
-		{City: "Berlin", Temperature: 15,   Date: time.Now()},
+		{City: "Gdansk", Temperature: 15,   Date: time.Now()},
 		{City: "Warsaw", Temperature: 18.3, Date: time.Now()},
-		{City: "Paris",  Temperature: 20.1, Date: time.Now()},
-		{City: "Rome",   Temperature: 23.7, Date: time.Now()},
-		{City: "London", Temperature: 16.8, Date: time.Now()},
-		{City: "Madrid", Temperature: 25.4, Date: time.Now()},
+		{City: "Krakow",  Temperature: 20.1, Date: time.Now()},
 	}
 
 	for _, w := range weather {
