8eedb3066083013c6cad76329045d89fa30f486f
viashchuk <victoriya.dev@icloud.com>
2025-06-06 13:12:51 +0200

    Add views

--- task-7/Sources/Task/Controllers/ProductController.swift
+++ task-7/Sources/Task/Controllers/ProductController.swift
@@ -6,52 +6,107 @@ struct ProductController: RouteCollection {
         let products = routes.grouped("products")
 
         products.get(use: index)
+        products.get("new", use: new)
         products.post(use: create)
 
         products.group(":id") { product in
             product.get(use: show)
-            product.put(use: update)
-            product.delete(use: delete)
+            product.get("edit", use: edit)
+            product.post("update", use: update)
+            product.post("delete", use: delete)
         }
     }
 
     @Sendable
-    func index(req: Request) async throws -> [Product] {
-        try await Product.query(on: req.db).all()
+    func index(req: Request) throws -> EventLoopFuture<View> {
+        return Product.query(on: req.db).all().flatMap { products in
+            let context = ["products": products]
+            return req.view.render("products/index", context)
+        }
     }
 
-    @Sendable
-    func create(req: Request) async throws -> Product {
+    func new(req: Request) throws -> EventLoopFuture<View> {
+        return req.view.render("products/new")
+    }
+
+    func create(req: Request) throws -> EventLoopFuture<Response> {
         let product = try req.content.decode(Product.self)
-        try await product.save(on: req.db)
-        return product
+        
+        return product.save(on: req.db).map {
+            req.redirect(to: "/products")
+        }
+    }
+    
+    @Sendable
+    func show(req: Request) throws -> EventLoopFuture<View> {
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+
+        return Product.find(id, on: req.db).flatMap { product in
+            guard let product = product else {
+                return req.eventLoop.future(error: Abort(.notFound))
+            }
+
+            let context = ["product": product]
+            return req.view.render("products/show", context)
+        }
     }
 
     @Sendable
-    func show(req: Request) async throws -> Product {
-        guard let product = try await Product.find(req.parameters.get("id"), on: req.db) else {
-            throw Abort(.notFound)
+    func edit(req: Request) throws -> EventLoopFuture<View> {
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest)
         }
-        return product
+
+        return Product.find(id, on: req.db).flatMap { product in
+            guard let product = product else {
+                return req.eventLoop.future(error: Abort(.notFound))
+            }
+
+            let context = ["product": product]
+            return req.view.render("products/edit", context)
+        }
+    }
+
+    func update(req: Request) throws -> EventLoopFuture<Response> {
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+
+        return Product.find(id, on: req.db).flatMap { product in
+            guard let product = product else {
+                return req.eventLoop.future(error: Abort(.notFound))
+            }
+
+            do {
+                let updatedProduct = try req.content.decode(Product.self)
+                product.title = updatedProduct.title
+                product.price = updatedProduct.price
+
+                return product.save(on: req.db).map {
+                    req.redirect(to: "/products/\(id)")
+                }
+            } catch {
+                return req.eventLoop.future(error: error)
+            }
+        }
     }
 
     @Sendable
-    func update(req: Request) async throws -> Product {
-        guard let product = try await Product.find(req.parameters.get("id"), on: req.db) else {
-            throw Abort(.notFound)
+    func delete(req: Request) throws -> EventLoopFuture<Response> {
+        guard let id = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest)
         }
-        let updatedProduct = try req.content.decode(Product.self)
-        product.title = updatedProduct.title
-        try await product.save(on: req.db)
-        return product
-    }
 
-    @Sendable
-    func delete(req: Request) async throws -> HTTPStatus {
-        guard let product = try await Product.find(req.parameters.get("id"), on: req.db) else {
-            throw Abort(.notFound)
+        return Product.find(id, on: req.db).flatMap { product in
+            guard let product = product else {
+                return req.eventLoop.future(error: Abort(.notFound))
+            }
+
+            return product.delete(on: req.db).map {
+                req.redirect(to: "/products")
+            }
         }
-        try await product.delete(on: req.db)
-        return .ok
     }
 }
